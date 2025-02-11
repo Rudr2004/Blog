dff2495221ebb59b9b39387694229018c6a7bd41
Rudr2004 <rudrajan24@gmail.com>
2025-02-11 18:40:43 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,18 @@ const PORT = process.env.PORT || 6000;
 
 app.use(
   cors({
-    // origin: ["http://localhost:5173", "https://baaplg.netlify.app/"],
-    origin: "*",
+    origin: ["http://localhost:5173", "https://baaplg.netlify.app/"],
+    //origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 app.use(express.json());
 app.use("/api/blog", router);
